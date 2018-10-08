6d35c3fd9280f62b1094aac361aee2d2325a9c17
Sabbath Troisi <SABBATHT@amdocs.com>
2018-10-08 11:26:06 -0400

    Fixed bug in metrics.py, period_in_secs config param was being ignored (hardcoded 120sec period was being used)

--- src/metrics.py
+++ src/metrics.py
@@ -12,15 +12,14 @@ def get_incoming_bytes(stream, period_in_secs):
 
 def get_metric_sum(stream, period_in_secs, metric_name):
     data_points = cloudwatch.get_metric_statistics(
-        Period     = 120,
-        StartTime  = datetime.datetime.utcnow() - datetime.timedelta(seconds=120),
+        Period     = period_in_secs,
+        StartTime  = datetime.datetime.utcnow() - datetime.timedelta(seconds=period_in_secs),
         EndTime    = datetime.datetime.utcnow(),
         MetricName = metric_name,
         Namespace  = 'AWS/Kinesis',
         Statistics = ['Sum',],
         Dimensions = [{'Name':'StreamName', 'Value':stream["name"]}]
     )    
-    # {u'Datapoints': [{u'Timestamp': datetime.datetime(2018, 9, 28, 19, 6, tzinfo=tzlocal()), u'Average': 1.0, u'Sum': 4.0, u'Unit': 'Count'}], 'ResponseMetadata': {'RetryAttempts': 0, 'HTTPStatusCode': 200, 'RequestId': 'efc08917-c351-11e8-b767-a1f10b71814b', 'HTTPHeaders': {'x-amzn-requestid': 'efc08917-c351-11e8-b767-a1f10b71814b', 'date': 'Fri, 28 Sep 2018 19:08:51 GMT', 'content-length': '519', 'content-type': 'text/xml'}}, u'Label': 'IncomingRecords'}
     if data_points["Datapoints"] and len(data_points["Datapoints"]) > 0:
         return data_points["Datapoints"][0]["Sum"]
     else:
