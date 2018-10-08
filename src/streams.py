870c928b70869ec3d0b6e90994a575b767a25308
Sabbath Troisi <SABBATHT@amdocs.com>
2018-10-08 11:25:34 -0400

    Code cleanup/removing unecessary comments

--- src/streams.py
+++ src/streams.py
@@ -31,15 +31,6 @@ def get_current_status_and_shard_count(stream):
             break
     return (stream_status, shard_counter)
 
-# This operation has the following default limits. By default, you cannot do the following:
-#    Scale more than twice per rolling 24-hour period per stream
-#    Scale up to more than double your current shard count for a stream
-#    Scale down below half your current shard count for a stream
-#    Scale up to more than 500 shards in a stream
-#    Scale a stream with more than 500 shards down unless the result is less than 500 shards
-#    Scale up to more than the shard limit for your account
-# See: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesis.html#client    
-#
 def update_shard_count(stream, target_shard_count):
     update_response = kinesis.update_shard_count(
         StreamName = stream["name"],
